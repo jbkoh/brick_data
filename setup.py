32be737d8ba04a97873b7c61e08cc48a8a58c6e9
Jason Koh <bk7749@gmail.com>
2019-01-06 21:07:34 -0800

    packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from pkg_resources import parse_requirements
+
+__author__ = 'Jason Koh'
+__version__ = '0.0.1'
+
+install_reqs = parse_requirements(open('requirements.txt'))
+reqs = [ir.name for ir in install_reqs]
+
+setup(
+    name = 'brick_data',
+    version = __version__,
+    packages = find_packages(),
+    description = 'A wrapper of Timeseries DBs',
+    install_requires = reqs,
+)
+
