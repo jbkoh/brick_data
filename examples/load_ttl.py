d7112f48154a20ab729d8708e6a6bfa13f677814
Jason Koh <bk7749@gmail.com>
2019-03-11 17:04:10 -0700

    fix version

--- examples/load_ttl.py
+++ examples/load_ttl.py
@@ -1,5 +1,5 @@
 from brick_data.sparql import BrickSparql
 
 
-brick_endpoint = BrickSparql('http://localhost:8890/sparql', '1.0.3')
+brick_endpoint = BrickSparql('http://localhost:8890/sparql', '1.0.2')
 brick_endpoint.load_rdffile('ebu3b_brick.ttl')
