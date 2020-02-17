741f1f91afdfa592f916791ccd2b63f9af70a9b2
Jason Koh <bk7749@gmail.com>
2020-02-17 07:31:11 +0000

    basic example for async

---
+++ examples/basic_async.py
@@ -0,0 +1,38 @@
+import asyncio
+from asyncio import FIRST_COMPLETED, ALL_COMPLETED
+import pdb
+
+from brick_data.sparql import BrickSparqlAsync
+
+brick_endpoint = BrickSparqlAsync('http://bd-testbed.ucsd.edu:8890/sparql',
+                             '1.0.3',
+                             graph='http://example.com',
+                             base_ns='http://example.com#',
+                             load_schema=False,
+                             )
+
+qstr = """
+select ?s ?p ?o where {
+?s ?p ?o.
+}
+"""
+brick_endpoint.query(qstr)
+
+
+loop = asyncio.get_event_loop()
+
+
+tasks = []
+#tasks.append(brick_endpoint.query(qstr))
+fut = brick_endpoint.add_triple(
+    'http://example.com#aaa',
+    'http://example.com#bbb',
+    'http://example.com#ccc',
+)
+tasks.append(fut)
+res = loop.run_until_complete(asyncio.gather(*tasks))
+
+
+loop.close()
+pdb.set_trace()
+
