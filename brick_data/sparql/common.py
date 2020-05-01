043a356a621b8e0d3e40e87de24e123d999b2175
Jason Koh <bk7749@gmail.com>
2020-05-01 05:11:25 +0000

    add a missing def

--- brick_data/sparql/common.py
+++ brick_data/sparql/common.py
@@ -4,6 +4,11 @@ import semver
 
 VIRTUOSO = 'virtuoso'
 
+def _append_patch_ver(version):
+    if len(version.split(".")) < 3:
+        version += ".0"
+    return version
+
 
 def semver_compare(src_version, target_version):
     src_version = _append_patch_ver(src_version)
